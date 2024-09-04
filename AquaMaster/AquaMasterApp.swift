435ba846838e1492f5853fe049ce36b80033a56c
Ali-jfry <alijfry029@gmail.com>
2024-09-04 15:08:27 +1000

    Initial Commit

---
+++ AquaMaster/AquaMasterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AquaMasterApp.swift
+//  AquaMaster
+//
+//  Created by Ali Agha Jafari on 4/9/2024.
+//
+
+import SwiftUI
+
+@main
+struct AquaMasterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
