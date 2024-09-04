ac34436990feaaa5849c41687ae4a756b4c8c2dc
Ali-jfry <alijfry029@gmail.com>
2024-09-04 15:18:14 +1000

    Testing github

--- AquaMaster/ContentView.swift
+++ AquaMaster/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundStyle(.tint)
             Text("Hello, world!")
+            // this is a testing for github
         }
         .padding()
     }
